34e87a641785ff9a275d9029582cb9b6dc539310
jfavier <jfavier@appannie.com>
2020-05-07 16:45:25 +0200

    [current-location]
    - Introduced a viewModel to handle the fine location permission request

--- app/src/main/java/fr/jorisfavier/venuesaroundme/RestaurantsMapsActivity.kt
+++ app/src/main/java/fr/jorisfavier/venuesaroundme/RestaurantsMapsActivity.kt
@@ -1,43 +1,91 @@
 package fr.jorisfavier.venuesaroundme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Observer
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class RestaurantsMapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class RestaurantsMapsActivity : AppCompatActivity() {
 
-    private lateinit var mMap: GoogleMap
+    companion object {
+        const val FINE_LOCATION_PERMISSION_ID = 54
+    }
+
+    private lateinit var map: GoogleMap
+
+    private val viewModel: RestaurantsMapsViewModel by viewModels()
+
+    private val isFineLocationGranted: Boolean
+        get() =
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { map = it }
+        initObservers()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.setFineLocationGranted(isFineLocationGranted)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        viewModel.setFineLocationGranted(
+            requestCode == FINE_LOCATION_PERMISSION_ID && grantResults.isNotEmpty()
+                    && grantResults.first() == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    private fun initObservers() {
+        viewModel.state.observe(this, Observer { state ->
+            if (state == RestaurantsMapsViewModel.State.FineLocationNotGranted) {
+                requestFineLocationPermission()
+            } else {
+                initMap()
+            }
+        })
     }
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * If the fine location has not been granted a native dialog will be prompt to the user
+     * asking for the permission
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    private fun requestFineLocationPermission() {
+        MaterialAlertDialogBuilder(this)
+            .setMessage(R.string.we_need_fine_location_permission)
+            .setNegativeButton(android.R.string.cancel) { _, _ -> finish() }
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                if (!isFineLocationGranted) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        FINE_LOCATION_PERMISSION_ID
+                    )
+                }
+            }
+            .show()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun initMap() {
+        map.isMyLocationEnabled = true
+        map.uiSettings.isMyLocationButtonEnabled = true
     }
 }
